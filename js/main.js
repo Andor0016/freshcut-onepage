dcd8b19526f818398c8c7c131da1211c579f0de0
Balo Andor <balo.andor@xpericad.hu>
2024-10-22 23:28:24 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('footer-instagram').addEventListener('click', () => openInstagram());
+
+
+    function openInstagram()
+    {
+        window.open('https://www.instagram.com/andor.0016', '_blank');
+    }
 });
