f410c57941f641b21a49d78baae759767a9d9d8d
Balo Andor <balo.andor@xpericad.hu>
2024-10-23 21:59:58 +0200

    translated properties and created js for change

--- js/main.js
+++ js/main.js
@@ -42,9 +42,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function changeProperties(data)
     {
+        document.getElementById('contact-header').innerHTML = data.contactHeader;
+        document.getElementById('opening').innerHTML = data.opening;
+        document.getElementById('monday').innerHTML = data.monday;
+        document.getElementById('tuesday').innerHTML = data.tuesday;
+        document.getElementById('wednesday').innerHTML = data.wednesday;
+        document.getElementById('thursday').innerHTML = data.thursday;
+        document.getElementById('friday').innerHTML = data.friday;
+        document.getElementById('saturday').innerHTML = data.saturday;
+        document.getElementById('sunday').innerHTML = data.sunday;
+        Array.from(document.getElementsByClassName('closed')).forEach((element) => {
+            element.innerHTML = data.closed;
+        });
+        document.getElementById('address-first-line').innerHTML = data.addressFirstLine;
+        document.getElementById('address-second-line').innerHTML = data.addressSecondLine;
         document.getElementById('content-language-label').innerHTML = data.contentLanguage;
         document.getElementById('hu-HU').innerHTML = data.huHU;
         document.getElementById('sr-SR').innerHTML = data.srSR;
+        document.getElementById('footer-call-us').alt = data.footerCallAlt;
+        document.getElementById('footer-call-us').title = data.footerCallTitle;
+        document.getElementById('footer-mail-us').alt = data.footerMailAlt;
+        document.getElementById('footer-mail-us').title = data.footerMailTitle;
+        document.getElementById('footer-facebook').alt = data.footerFacebookAlt;
+        document.getElementById('footer-facebook').title = data.footerFacebookTitle;
+        document.getElementById('footer-instagram').alt = data.footerInstagramAlt;
+        document.getElementById('footer-instagram').title = data.footerInstagramTitle;
     }
 
     function callNumber()
