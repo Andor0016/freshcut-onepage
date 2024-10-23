d4079a68b3eb1066bfe2486bef074c446718a54d
Balo Andor <balo.andor@xpericad.hu>
2024-10-23 20:35:32 +0200

    test is js fetch working on gh pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     let currentLanguage = 'hu-HU';
+    //contact elements
+    document.getElementById('mail').addEventListener('click', () => openMail());
+    document.getElementById('number').addEventListener('click', () => callNumber);
+    document.getElementById('cont-insta').addEventListener('click', () => openInstagram());
+    document.getElementById('cont-fb').addEventListener('click', () => openFacebook());
     //footer elements
     document.getElementById('content-language').addEventListener('change', (event) => changeLanguage(event.target.value));
-    document.getElementById('footer-call-us').addEventListener('click', () => {});
+    document.getElementById('footer-call-us').addEventListener('click', () => callNumber());
     document.getElementById('footer-mail-us').addEventListener('click', () => openMail());
     document.getElementById('footer-facebook').addEventListener('click', () => openFacebook());
     document.getElementById('footer-instagram').addEventListener('click', () => openInstagram());
@@ -12,8 +17,39 @@ document.addEventListener('DOMContentLoaded', () => {
     {
         if(targetLanguage !== currentLanguage)
         {
+            currentLanguage = targetLanguage;
+            let filePath = '';
+            if(targetLanguage === 'hu-HU'){
+                filePath = '../resources/i18n/hu-HU.json';
+            }
+            else if(targetLanguage === 'sr-SR'){
+                filePath = '../resources/i18n/sr-SR.json';
+            }
 
+            fetch(filePath)
+                .then(response => {
+                    if(!response.ok){
+                        alert('Language change error!');
+                    }
+                    return response.json();
+                })
+                .then(data => changeProperties(data))
+                .catch(error => {
+                    alert(error);
+                });
         }
+    }
+
+    function changeProperties(data)
+    {
+        document.getElementById('content-language-label').innerHTML = data.contentLanguage;
+        document.getElementById('hu-HU').innerHTML = data.huHU;
+        document.getElementById('sr-SR').innerHTML = data.srSR;
+    }
+
+    function callNumber()
+    {
+        //not implemented yet
     }
 
     function openMail()
